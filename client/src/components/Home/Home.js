cfaee9dc9105218c0f5175279e7d61200dcf3dee
missmahi55 <mahakagarwal248@gmail.com>
2022-05-01 00:27:49 +0530

    image

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import style from './Home.module.css'
 import Footer from "../Footer/Footer";
 import Mycomp from "../myconponent/Mycomp";
 
 const Home = () => {
   return (
     <>
+    <div className={style.tvyomhomepage}></div>
       <Mycomp />
       <Footer />
     </>
